ece68e62d8bd3578b194556a3418c08cad48b2df
Vishu0106 <kattameedivishnu2003@gmail.com>
2024-04-30 14:26:16 +0530

    Added react-toaster to main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,7 @@ import App from './App.jsx'
 import { BrowserRouter } from 'react-router-dom'
 import {Provider} from 'react-redux';
 import { store } from './redux/store.js';
+import { Toaster } from 'react-hot-toast';
 
 import './index.css'
 
@@ -12,6 +13,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
     <Provider store={store}>
       <BrowserRouter>
         <App />
+        <Toaster/>
       </BrowserRouter>
     </Provider>
   </React.StrictMode>,
